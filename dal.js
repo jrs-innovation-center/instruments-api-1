c10a989f6cb2b2343f33d6394aec6d090d1644e1
tripott <tripott@gmail.com>
2018-06-19 14:04:32 -0400

    DELETE \instrument\:id

--- dal.js
+++ dal.js
@@ -16,13 +16,27 @@ const db = new PouchDB(
 const getInstrument = (id, callback) => db.get(id, callback)
 
 const addInstrument = (instrument, callback) => {
-  console.log('HI Im inside addInstrument')
-
   const modifiedInstrument = merge(instrument, {
     type: 'instrument',
     _id: pkGen('instrument', '_', `${instrument.category} ${instrument.name}`)
   })
   db.put(modifiedInstrument, callback)
+}
+
+const deleteInstrument = (instrumentID, callback) => {
+  db.get(instrumentID, function(err, doc) {
+    if (err) {
+      callback(err)
+      return
+    }
+    db.remove(doc, function(err, deleteResult) {
+      if (err) {
+        callback(err)
+        return
+      }
+      callback(null, deleteResult)
+    })
+  })
 }
 
 ///////////////////////////
@@ -36,7 +50,8 @@ function getDoc(id, callback) {
 
 const dal = {
   getInstrument,
-  addInstrument
+  addInstrument,
+  deleteInstrument
 }
 
 module.exports = dal
